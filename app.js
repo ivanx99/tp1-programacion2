594b7daef9febc420314c51732b0c871dfe34ebd
naileiva <nailenleiva@gmail.com>
2026-09-25 03:33:23 -0300

    realice el styles.css

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("Aplicación inicializada");
+
+document.addEventListener('DOMContentLoaded', () => {
+    const btnHambuerguesa = document.querySelector ('.btn-hamburguesa');
+    const menuLista = document.getElementById('menu-lista');
+    if (btnHambuerguesa && menuLista) {
+        btnHambuerguesa.addEventListener('click', () => {
+            menuLista.classList.toggle('mostrar');
+        });
+    }
+});
